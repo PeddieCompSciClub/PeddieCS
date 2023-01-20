1470a6a5fbd6bad5d0fb40e4277c819a45c19eb9
PeddieCompSciClub <compsciclub@peddie.org>
2023-01-20 13:57:06 -0500

    working on thing to add stuff to database

--- nodejs/database.js
+++ nodejs/database.js
@@ -1,1 +1,27 @@
-test
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "admincs",
+    password: "BeatBlair1864",
+    database: "pets",
+    port: 3306
+});
+
+function parseJSON(json) {
+    return JSON.parse(json);
+}
+
+function addMember(firstName, lastName, email) {
+    con.connect(function (err) {
+        if (err) throw err;
+        console.log("Connected!");
+        var sql = "INSERT INTO `cats` (`id`, `name`, `owner`, `birth`) VALUES ";
+        var str = sql + "(" + "\'" + firstName + "\', " + "\'" + lastName + "\', " + "\'" + email + "\'" + ");";
+        console.log(str);
+        con.query(str, function (err, result) {
+            if (err) throw err;
+            console.log("Number of records inserted: " + result.affectedRows);
+        });
+    });
+}
