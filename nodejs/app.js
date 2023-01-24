0c74cf955e22e6cd39ab18ba68f7fb7a9b08f76f
Tomaz Chevres <tchevres-24@peddie.org>
2023-01-24 11:54:55 -0500

    Figuring out jQuery

--- nodejs/app.js
+++ nodejs/app.js
@@ -2,32 +2,34 @@ var mysql = require('mysql');
 var express = require('express');
 var router = express.Router();
 
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "admincs",
-    password: "BeatBlair1864",
-    database: "pets",
-    port: 3306
-});
+// var con = mysql.createConnection({
+//     host: "localhost",
+//     user: "admincs",
+//     password: "BeatBlair1864",
+//     database: "pets",
+//     port: 3306
+// });
 
-function parseJSON(json) {
-    return JSON.parse(json);
-}
+// // function parseJSON(json) {
+// //     return JSON.parse(json);
+// // }
 
-function addMember(firstName, lastName, email) {
-    con.connect(function (err) {
-        if (err) throw err;
-        console.log("Connected!");
-        var sql = "INSERT INTO `cats` (`id`, `name`, `owner`, `birth`) VALUES ";
-        var str = sql + "(" + "\'" + firstName + "\', " + "\'" + lastName + "\', " + "\'" + email + "\'" + ");";
-        console.log(str);
-        con.query(str, function (err, result) {
-            if (err) throw err;
-            console.log("Number of records inserted: " + result.affectedRows);
-        });
-    });
-}
+// // function addMember(firstName, lastName, email) {
+// //     con.connect(function (err) {
+// //         if (err) throw err;
+// //         console.log("Connected!");
+// //         var sql = "INSERT INTO `cats` (`id`, `name`, `owner`, `birth`) VALUES ";
+// //         var str = sql + "(" + "\'" + firstName + "\', " + "\'" + lastName + "\', " + "\'" + email + "\'" + ");";
+// //         console.log(str);
+// //         con.query(str, function (err, result) {
+// //             if (err) throw err;
+// //             console.log("Number of records inserted: " + result.affectedRows);
+// //         });
+// //     });
+// // }
 
-router.post('/login', function(req, res) {
-
+router.post('/test', function(req, res) {
+    console.log("reached test method")
+    res.json({"message":"success"});
+    res.end();
 });
