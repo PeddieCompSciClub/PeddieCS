e0c692e1c978fce8f4aa9120f11db3e4d29bb382
PeddieCompSciClub <compsciclub@peddie.org>
2023-02-02 14:43:02 -0500

    test function

--- nodejs/app.js
+++ nodejs/app.js
@@ -2,34 +2,45 @@ var mysql = require('mysql');
 var express = require('express');
 var router = express.Router();
 
-// var con = mysql.createConnection({
-//     host: "localhost",
-//     user: "admincs",
-//     password: "BeatBlair1864",
-//     database: "pets",
-//     port: 3306
-// });
-
-// // function parseJSON(json) {
-// //     return JSON.parse(json);
-// // }
-
-// // function addMember(firstName, lastName, email) {
-// //     con.connect(function (err) {
-// //         if (err) throw err;
-// //         console.log("Connected!");
-// //         var sql = "INSERT INTO `cats` (`id`, `name`, `owner`, `birth`) VALUES ";
-// //         var str = sql + "(" + "\'" + firstName + "\', " + "\'" + lastName + "\', " + "\'" + email + "\'" + ");";
-// //         console.log(str);
-// //         con.query(str, function (err, result) {
-// //             if (err) throw err;
-// //             console.log("Number of records inserted: " + result.affectedRows);
-// //         });
-// //     });
-// // }
-
-router.post('/test', function(req, res) {
-    console.log("reached test method")
-    res.json({"message":"success"});
-    res.end();
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "admincs",
+    password: "BeatBlair1864",
+    database: "pets",
+    port: 3306
 });
+
+function connect() {
+    con.connect(function (err) {
+        if (err) throw err;
+        console.log("Connected!");
+    });
+}
+
+function test(){
+    console.log("Tested");
+}
+
+// function parseJSON(json) {
+//     return JSON.parse(json);
+// }
+
+// function addMember(firstName, lastName, email) {
+//     con.connect(function (err) {
+//         if (err) throw err;
+//         console.log("Connected!");
+//         var sql = "INSERT INTO `cats` (`id`, `name`, `owner`, `birth`) VALUES ";
+//         var str = sql + "(" + "\'" + firstName + "\', " + "\'" + lastName + "\', " + "\'" + email + "\'" + ");";
+//         console.log(str);
+//         con.query(str, function (err, result) {
+//             if (err) throw err;
+//             console.log("Number of records inserted: " + result.affectedRows);
+//         });
+//     });
+// }
+
+// router.post('/test', function(req, res) {
+//     console.log("reached test method")
+//     res.json({"message":"success"});
+//     res.end();
+// });
