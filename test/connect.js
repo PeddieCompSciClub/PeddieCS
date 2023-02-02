4c88b2a07ca863f484862955e8e3f2f9167ef71d
PeddieCompSciClub <compsciclub@peddie.org>
2023-02-02 14:49:25 -0500

    test

---
+++ test/connect.js
@@ -0,0 +1,18 @@
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "admincs",
+    password: "BeatBlair1864",
+    database: "pets",
+    port: 3306
+});
+
+function connect() {
+    con.connect(function (err) {
+        if (err) throw err;
+        console.log("Connected!");
+    });
+}
+
+function test(){
+    console.log("Tested");
+}
