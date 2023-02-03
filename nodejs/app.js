3a21878ec08c72d5112281d2b1746f61922231ad
PeddieCompSciClub <compsciclub@peddie.org>
2023-02-03 13:37:14 -0500

    test nodejs

--- nodejs/app.js
+++ nodejs/app.js
@@ -18,6 +18,11 @@ function connect() {
     con.connect(function (err) {
         if (err) throw err;
         console.log("Connected!");
+
+        con.query("SELECT * FROM customers", function (err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+        });
     });
 }
 
