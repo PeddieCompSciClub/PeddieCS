c115df170ea6d0c699d9cb8fcd4b475846cc1617
Tomaz Chevres <tchevres-24@peddie.org>
2023-02-06 19:50:10 -0500

    add arguments through command line

--- nodejs/app.js
+++ nodejs/app.js
@@ -12,11 +12,19 @@ var con = mysql.createConnection({
 
 console.log("RUN")
 
-addMember("Ananya", "Hari", "ahari-25@peddie.org", 2025);
-await delay(1000);
-addMember("Sophia","Wu","swu-24@peddie.org",2024);
-await delay(1000);
-addMember("Ryan","Rong","rrong-24@peddie.org",2024);
+// addMember("Ananya", "Hari", "ahari-25@peddie.org", 2025);
+// addMember("Sophia", "Wu", "swu-24@peddie.org", 2024);
+// addMember("Ryan", "Rong", "rrong-24@peddie.org", 2024);
+
+/*
+    Allows user to pass arguments through command line
+    for example:
+    $ node app.js addMember Tomaz Chevres tchevres-24@peddie.org 2024
+    will have values of "addMember" "Tomaz" "Chevres" "tchevres-24@peddie.org" 2024
+*/
+process.argv.forEach((value, index) => {
+    console.log(index, value);
+});
 
 function read() {
     con.connect(function (err) {
@@ -35,7 +43,7 @@ function addMember(first_name, last_name, email, year) {
     con.connect(function (err) {
         if (err) throw err;
         console.log("Connected!");
-        var sql = "INSERT INTO members (first_name, last_name, email, year) VALUES ('"+first_name+"', '"+last_name+"', '"+email+"', "+year+")";
+        var sql = "INSERT INTO members (first_name, last_name, email, year) VALUES ('" + first_name + "', '" + last_name + "', '" + email + "', " + year + ")";
         con.query(sql, function (err, result) {
             if (err) throw err;
             console.log("1 record inserted");
