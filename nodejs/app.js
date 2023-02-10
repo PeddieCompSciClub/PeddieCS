dc023882da0dae9e2dc603cf16b5edbccb0c9fc8
Tomaz Chevres <tchevres-24@peddie.org>
2023-02-09 20:53:41 -0500

    added express & changed some var -> const

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,5 +1,6 @@
-var mysql = require('mysql');
-// var express = require('express');
+const mysql = require('mysql');
+const express = require('express');
+const app = express();
 // var router = express.Router();
 
 var con = mysql.createConnection({
@@ -10,27 +11,31 @@ var con = mysql.createConnection({
     port: 3306
 });
 
+
+
 /*
-    Allows user to pass arguments through command line
-    input:
-    $ node app.js debug Tomaz Chevres tchevres-24@peddie.org 2024
-    output:
-    0 '/usr/bin/node'
-    1 '/var/www/CSProjects/nodejs/app.js'
-    2 'debug'
-    3 'Tomaz'
-    4 'Chevres'
-    5 'tchevres-24@peddie.org'
-    6 '2024'
-*/
-switch (process.argv[2]){
+ * The most important part of the code :), this tells express to listen on port 5622 of the local host
+ * (our proxy will route the requests to us, if you need a refresher on that just look through the docs)
+ */
+app.listen(port, () => {
+    console.log(`Server running on port${port}`);
+});
+
+/*
+ *  Allows user to pass arguments through command line (mainly for debugging)
+ *  for example:
+ *  $ node app.js addMember Tomaz Chevres tchevres-24@peddie.org 2024
+ *  sets process.argv to ['user/bin/node','/var/www/CSProjects/nodejs/app.js','addMember','Tomaz','Chevres','tchevres-24@peddie.org','2024']
+ *  and adds the member to the database
+ */
+switch (process.argv[2]) {
     case 'debug':
         process.argv.forEach((value, index) => {
             console.log(index, value);
-        });  
+        });
         break;
     case 'addMember':
-        addMember(process.argv[3],process.argv[4],process.argv[5],parseInt(process.argv[6]));
+        addMember(process.argv[3], process.argv[4], process.argv[5], parseInt(process.argv[6]));
         break;
 }
 
@@ -57,7 +62,7 @@ function addMember(first_name, last_name, email, year) {
         var sql = "INSERT INTO members (first_name, last_name, email, year) VALUES ('" + first_name + "', '" + last_name + "', '" + email + "', " + year + ")";
         con.query(sql, function (err, result) {
             if (err) throw err;
-            console.log(first_name+" "+last_name+" added to members");
+            console.log(first_name + " " + last_name + " added to members");
         });
         con.end();
     });
