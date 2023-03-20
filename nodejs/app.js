cf8ce468bbced881e1be41b60f0c432950068c8f
PeddieCompSciClub <compsciclub@peddie.org>
2023-03-20 08:50:49 -0400

    updating nodeJS

--- nodejs/app.js
+++ nodejs/app.js
@@ -117,7 +117,7 @@ app.post('/submitMember', function (req, res) {
         con.connect(function (err) {
             if (err) throw err;
             //inserts user data into the table, overwritting it if there is already an entry with the same email
-            var sql = "INSERT INTO tempMembers (first_name, last_name, email, year, verificationCode) VALUES ('" + firstName + "', '" + lastName + "', '" + email + "', " + year + ", '" + verificationCode + "') ON DUPLICATE KEY UPDATE first_name='" + firstName + "', last_name='" + lastName + "', year=" + year + ", verificationCode='" + verificationCode + "'";
+            var sql = "INSERT INTO members (first_name, last_name, email, year, verificationCode) VALUES ('" + firstName + "', '" + lastName + "', '" + email + "', " + year + ", '" + verificationCode + "') ON DUPLICATE KEY UPDATE first_name='" + firstName + "', last_name='" + lastName + "', year=" + year + ", verificationCode='" + verificationCode + "'";
             con.query(sql, function (err, result) {
                 if (err) throw err;
                 console.log(firstName + " " + lastName + " added to tempMembers");
@@ -190,27 +190,37 @@ app.post('/addMember', function (req, res) {
 
         con.connect(function (err) {
             if (err) throw err;
-            con.query("SELECT * FROM tempMembers WHERE verificationCode=" + "\"" + String(verificationCode) + "\";", function (err, result, fields) {
-                if (err) throw err;
-                if (result[0]==null || result[0].email != email) {
-                    res.send({ "error": true, "message": "Email and Verification Code do not match." });
-                } else {
-                    //on success, add data to members, and remove from tempMembers
-                    var user = result[0];
-                    var sql = "INSERT INTO members (first_name, last_name, email, year) VALUES ('" + user.first_name + "', '" + user.last_name + "', '" + user.email + "', " + user.year + ") ON DUPLICATE KEY UPDATE first_name='" + user.first_name + "', last_name='" + user.last_name + "', year=" + user.year;
-                    con.query(sql, function (err, result) {
-                        if (err) throw err;
-
-                        //if everything is still going fine, delete the entry from tempMembers
-                        con.query('DELETE FROM tempMembers WHERE verificationCode="' + String(verificationCode + '";'), function (err, result) {
-                            if (err) throw err;
-
-                            res.send({ "error": false, "message": "Successfully made " + user.first_name + " a member." });
-                            con.end();
-                        });
-                    });
+            con.query(`UPDATE members SET status = 1, verificationCode = '00000000' WHERE verificationCode = "${user.verificationCode}" AND email="${email}"`, function (err, result, fields){
+                if (err){
+                    throw err;
                 }
-            })
+                res.send({ "error": false, "message": "Successfully made " + user.first_name + " a member." });
+                con.end();
+            });
+
+
+            // con.query(`SELECT email FROM members WHERE verificationCode="${String(verificationCode)}" AND email=${email}`, function (err, result, fields) {
+            //     if (err) throw err;
+            //     if (result[0]==null) {
+            //         res.send({ "error": true, "message": "Email and Verification Code do not match." });
+            //     } else {
+            //         //on success, update the user's status to 1 (verified)
+            //         var user = result[0];
+            //         var sql = `UPDATE members SET status = 1, verificationCode = '00000000' WHERE verificationCode = ${user.verificationCode}`;
+            //         con.query(sql, function (err, result) {
+            //             if (err) throw err;
+            //             res.send({ "error": false, "message": "Successfully made " + user.first_name + " a member." });
+            //             //if everything is still going fine, delete the entry from tempMembers
+            //             // con.query('DELETE FROM tempMembers WHERE verificationCode="' + String(verificationCode + '";'), function (err, result) {
+            //             //     if (err) throw err;
+
+            //             //     res.send({ "error": false, "message": "Successfully made " + user.first_name + " a member." });
+            //             //     con.end();
+            //             // });
+            //             con.end;
+            //         });
+            //     }
+            // })
         })
 
         //move user image from temp to regular folder
