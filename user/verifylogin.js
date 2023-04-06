d6757a155b1f8b869fcbb1dce1b1c7e616e960d5
Tomaz Chevres <tchevres-24@peddie.org>
2023-04-06 14:55:14 -0400

    sign out

--- user/verifylogin.js
+++ user/verifylogin.js
@@ -26,9 +26,10 @@ function verifyLogin() {
 }
 
 function addSignoutButton(name) {
-    document.getElementById("navbar-main").appendChild(`<button class="user sign-out" onclick="removeCookie('credential'); window.location.href='/index.html'">${name}</button><style>.navbar-custom .user.sign-out::after,
-    .navbar-custom .user.sign-out::before {
-        content: "${name}";}</style>`);
+    const nav = document.getElementById("navbar-main");
+    nav.appendChild(`<button class="user sign-out" onclick="removeCookie('credential'); window.location.href='/index.html'">${name}</button>`);
+    nav.appendChild(`<style>.navbar-custom .user.sign-out::after,
+    .navbar-custom .user.sign-out::before {content: "${name}";}</style>`)
 }
 
 
