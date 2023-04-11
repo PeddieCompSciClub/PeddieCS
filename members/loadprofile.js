6e2425387e442454a058f975a4fed8ba2667c2e0
Tomaz Chevres <tchevres-24@peddie.org>
2023-04-11 16:41:43 -0400

    get projects

--- members/loadprofile.js
+++ members/loadprofile.js
@@ -6,9 +6,9 @@ getMember(email);
 
 //requests user data from the MySQL database
 function getMember(email) {
-    if(window.jQuery){
+    if (window.jQuery) {
         $.get("https://peddiecs.peddie.org/nodejs/getMemberData", {
-            "email":email
+            "email": email
         }, function (res) {
             if (res.message == "failed") {
                 console.log("Failed to get member data")
@@ -20,21 +20,26 @@ function getMember(email) {
     }
 }
 
-function displayMemberProfile(json){
-    var name = json.first_name +" "+ json.last_name;
+function displayMemberProfile(json) {
+    var name = json.first_name + " " + json.last_name;
     console.log("Loading data for " + name);
-    
+
     //add user display-icon
-    document.getElementById('name').innerText = name + (json.year != '0' ? (" '"+json.year.toString().slice(-2)) : '');
-    document.getElementById('image').src = 'user-images/'+username;
-    document.getElementById('info').innerHTML += `<li>${email}</li>` + (json.university?`<li>${json.university}</li>`:'');
+    document.getElementById('name').innerText = name + (json.year != '0' ? (" '" + json.year.toString().slice(-2)) : '');
+    // document.getElementById('image').src = 'user-images/'+username;
+    var img = document.getElementById('image');
+    img.src = 'user-images/' + username;
+    img.addEventListener('error', function () {
+        img.src = 'user-images/missing.jpg';
+    });
+    document.getElementById('info').innerHTML += `<li>${email}</li>` + (json.university ? `<li>${json.university}</li>` : '');
 
     //center icon
-    if(!json.bio && !json.groups){
+    if (!json.bio && !json.groups) {
         document.getElementById('icon').style = "grid-column:1/-1";
     } else {
-        if(json.bio){
-            document.getElementById('bio').innerHTML=`<h3>Bio</h3><p>${json.bio}</p>`
+        if (json.bio) {
+            document.getElementById('bio').innerHTML = `<h3>Bio</h3><p>${json.bio}</p>`
         }
 
     }
