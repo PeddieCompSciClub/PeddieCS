b43a1226d834c651a86e751710c7c60be1970e5d
Tomaz Chevres <tchevres-24@peddie.org>
2023-04-17 13:10:51 -0400

    update bio

--- user/profile.js
+++ user/profile.js
@@ -36,7 +36,8 @@ function displayMemberProfile(json) {
         document.getElementById('icon').style = "grid-column:1";
         //add bio
         if (json.bio) {
-            document.getElementById('bio').innerHTML = decodeURIComponent(json.bio).replace(/\n/g, '<br>');
+            // document.getElementById('bio').innerHTML = decodeURIComponent(json.bio).replace(/\n/g, '<br>');
+            document.getElementById('bio').innerHTML = 'ABC<br>DEF'
             document.getElementById('counter').innerText = `${json.bio.length}/1000`;
         }
         //add groups (not a thing yet)
