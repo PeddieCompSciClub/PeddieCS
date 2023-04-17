55c989ce1d249d263accde9124d6b12ecbb97f6e
PeddieCompSciClub <compsciclub@peddie.org>
2023-04-17 08:26:01 -0400

    Profile editor

--- user/profile.js
+++ user/profile.js
@@ -84,7 +84,7 @@ function displayMemberProfile(json) {
         timerId = setTimeout(function () {
             // Run your function here
             console.log("Function executed after 5 seconds of inactivity");
-            document.getElementById("status").innerText = "saving"
+            document.getElementById("status").innerText = "saving…"
         }, delay);
     });
 }
