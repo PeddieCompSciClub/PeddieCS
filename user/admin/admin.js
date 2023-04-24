2e1b09d5dc3f99f190da8bc78b572beb434a381a
Tomaz Chevres <tchevres-24@peddie.org>
2023-04-24 14:37:44 -0400

    admin page

--- user/admin/admin.js
+++ user/admin/admin.js
@@ -1,3 +1,21 @@
 function load(user){
     console.log(user);
+    const permissions = user.permissions.replace(' ','').split(',');
+    if(permissions.includes('admin')){
+        loadmembers(user);
+    }
+    else{
+        window.location.href = '/user/'
+    }
+}
+
+function loadmembers(){
+    $.get("https://peddiecs.peddie.org/nodejs/getAllMembers", {
+        }, function (res) {
+            if (res.message == "failed") {
+                console.log("Failed to get member data")
+            } else {
+                console.log(res);
+            }
+        });
 }
