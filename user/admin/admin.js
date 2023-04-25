dbdb9a1303197128a38a566a3c7aec4c7e01e7dd
Tomaz Chevres <tchevres-24@peddie.org>
2023-04-25 11:50:40 -0400

    admin script

--- user/admin/admin.js
+++ user/admin/admin.js
@@ -32,7 +32,7 @@ function loadmembers(){
                 document.getElementById("membersearch").addEventListener("input", function () {
                     var members = document.getElementsByClassName("memberbtn");
                     var search = document.getElementById("membersearch").value;
-                    console.log(value);
+                    console.log(search);
                 });
             }
         });
