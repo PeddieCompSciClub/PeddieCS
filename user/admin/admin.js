b695fb6812187acdf5dea864872d8b32e1bced17
Tomaz Chevres <tchevres-24@peddie.org>
2023-04-25 11:57:38 -0400

    admin script

--- user/admin/admin.js
+++ user/admin/admin.js
@@ -1,41 +1,43 @@
-function load(user){
-    const permissions = user.permissions.replace(' ','').split(',');
-    if(permissions.includes('admin')){
+function load(user) {
+    const permissions = user.permissions.replace(' ', '').split(',');
+    if (permissions.includes('admin')) {
         loadmembers(user);
     }
-    else{
+    else {
         window.location.href = '/user/'
     }
 }
 
 var memberSaved = true;
-function loadmembers(){
+function loadmembers() {
     $.get("https://peddiecs.peddie.org/nodejs/admin/getAllMembers", {
-            token: getCookie('credential')
-        }, function (res) {
-            if (res.message == "failed") {
-                console.log("Failed to get member data")
-            } else {
-                // console.log(res);
+        token: getCookie('credential')
+    }, function (res) {
+        if (res.message == "failed") {
+            console.log("Failed to get member data")
+        } else {
+            // console.log(res);
 
-                //add members to button list
-                for(let i=0; i<res.message.length; i++){
-                    var user = res.message[i];
-                    var button = `<button class="memberbtn" id="members_${user.email}">${user.first_name+' '+user.last_name}</button>`;
-                    document.getElementById(getGrade(user.year)).getElementsByClassName("memberlist")[0].innerHTML += button;
-                }
+            //add members to button list
+            for (let i = 0; i < res.message.length; i++) {
+                var user = res.message[i];
+                var button = `<button class="memberbtn" id="members_${user.email}">${user.first_name + ' ' + user.last_name}</button>`;
+                document.getElementById(getGrade(user.year)).getElementsByClassName("memberlist")[0].innerHTML += button;
+            }
 
-                //set active view active
-                document.getElementById('members_compsciclub@peddie.org').classList.add('active');
-                document.getElementById("membersearch").addEventListener("input", function () {
-                    var members = document.getElementsByClassName("memberbtn");
-                    var search = document.getElementById("membersearch").value;
-                    for(var i=0; i<members.length; i++){
+            //set active view active
+            document.getElementById('members_compsciclub@peddie.org').classList.add('active');
+            document.getElementById("membersearch").addEventListener("input", function () {
+                var members = document.getElementsByClassName("memberbtn");
+                var search = document.getElementById("membersearch").value;
+                for (var i = 0; i < members.length; i++) {
+                    if (members[i].innerText.includes(search) || members[i].id.includes(search)) {
                         console.log(members[i]);
                     }
-                });
-            }
-        });
+                }
+            });
+        }
+    });
 }
 
 function getGrade(year) {
