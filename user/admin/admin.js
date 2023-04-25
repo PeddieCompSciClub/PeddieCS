7e538af89fe4e4928424c62b24fd4f35fcf3d726
Tomaz Chevres <tchevres-24@peddie.org>
2023-04-25 11:27:28 -0400

    admin script

--- user/admin/admin.js
+++ user/admin/admin.js
@@ -40,3 +40,10 @@ function getGrade(year) {
     if (year - 3 == gradYear) return "Freshmen";
     return year;
 }
+
+//get the current school year (the graduation class of the seniors)
+function getCurrentYear() {
+    const d = new Date();
+    let year = d.getFullYear() + (d.getMonth() > 6 ? 1 : 0);
+    return year;
+}
