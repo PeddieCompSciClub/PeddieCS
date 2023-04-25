5cbe8d13b4e1efcf7472d4e592ad212924dd0597
Tomaz Chevres <tchevres-24@peddie.org>
2023-04-25 11:35:43 -0400

    admin script

--- user/admin/admin.js
+++ user/admin/admin.js
@@ -22,9 +22,11 @@ function loadmembers(){
                 //add members to button list
                 for(let i=0; i<res.message.length; i++){
                     var user = res.message[i];
-                    var button = `<button class="memberbtn">${user.first_name+' '+user.last_name}</button>`;
+                    var button = `<button class="memberbtn" id="${user.email}">${user.first_name+' '+user.last_name}</button>`;
                     document.getElementById(getGrade(user.year)).getElementsByClassName("memberlist")[0].innerHTML += button;
                 }
+
+                document.getElementById('compsciclub@peddie.org').classList.add('active');
             }
         });
 }
