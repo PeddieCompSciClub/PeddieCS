cdc5aac0110dc4b88f5f2c101918bd6e9bb9116c
Tomaz Chevres <tchevres-24@peddie.org>
2023-06-06 15:42:23 -0400

    load calendar popup

--- csfellows/script.js
+++ csfellows/script.js
@@ -54,7 +54,7 @@ function addCalendarEvents(year, month, data) {
     console.log({year:year,month:month,data:data});
     for(var i=0; i<data.length; i++){
         var event = data[i]
-        var eventDate = new Date(event.date);
+        var eventDate = new Date(event.date.substring(0,event.date.length-1));
         console.log(event.name,eventDate);
         document.getElementById('day-'+eventDate.getDate()).innerHTML += `<div class="event" style="background-color:${stringToColor(event.email)}; border-color:#00000000" onclick="loadPopup('${event.email}','${event.name}','${eventDate.getHours()}','${eventDate.getMinutes()}')">${event.name}</div>`;
     }
