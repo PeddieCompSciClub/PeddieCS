601720a2958dd630d0035429ec2e9075b9c0cc17
Tomaz Chevres <tchevres-24@peddie.org>
2023-06-06 16:07:26 -0400

    load calendar popup

--- user/admin/admin.js
+++ user/admin/admin.js
@@ -165,6 +165,6 @@ function getGrade(year) {
 //get the current school year (the graduation class of the seniors)
 function getCurrentYear() {
     const d = new Date();
-    let year = d.getFullYear() + (d.getMonth() > 6 ? 1 : 0);
+    let year = d.getFullYear() + (d.getMonth() >= 5 ? 1 : 0);
     return year;
 }
