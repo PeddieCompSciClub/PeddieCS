acd3f88b7d0082815d002a556faf9a743c846218
Tomaz Chevres <tchevres-24@peddie.org>
2023-06-09 22:53:57 -0400

    interact w/sign-up calendar

--- user/csfellows/script.js
+++ user/csfellows/script.js
@@ -171,7 +171,7 @@ function selectCalendarDate(element, date) {
     document.getElementById('fellows-preview').innerHTML = "";
     // console.log(loadedMonths);
     // console.log(loadedMonths.get([date.getFullYear(), date.getMonth()].toString()));
-    console.log(loadedMonths.get([date.getFullYear(), date.getMonth()].toString()),loadedMonths.get([date.getFullYear(), date.getMonth()].toString().length));
+    console.log(loadedMonths.get([date.getFullYear(), date.getMonth()].toString()),loadedMonths.get([date.getFullYear(), date.getMonth()].toString()).length);
     if (1>0){
         [].forEach.call(loadedMonths.get([date.getFullYear(), date.getMonth()].toString()), function (event) {
             eventDate = new Date(event.date.substring(0, event.date.length - 1));
