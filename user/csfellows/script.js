d2657e831f869868a7649a09aae60eb7f162e7a2
Tomaz Chevres <tchevres-24@peddie.org>
2023-06-10 23:12:31 -0400

    cancel calendar sign up

--- user/csfellows/script.js
+++ user/csfellows/script.js
@@ -210,7 +210,7 @@ function cancelEvent(id){
             if(document.getElementById('fellows-preview').childElementCount==0){
                 document.getElementById('fellows-preview').innerText = "(no fellows scheduled)"
             }
-            var events = loadedMonths.get([saveDate.getFullYear(), saveDate.getMonth()].toString()).data[0]
+            var events = loadedMonths.get([saveDate.getFullYear(), saveDate.getMonth()].toString()).data[0];
             console.log(events);
         }
     });
