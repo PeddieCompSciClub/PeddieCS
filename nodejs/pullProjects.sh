549ecedf974c610b3c0604e8de214be894c3fbcf
Tomaz Chevres <tchevres-24@peddie.org>
2023-07-03 22:16:06 -0400

    typo

---
+++ nodejs/pullProjects.sh
@@ -0,0 +1,7 @@
+# Use to pull live project/club pages from github
+# called every hour (later change to 1 per day)
+#
+#cd; cd /var/www/CSProjects/live/Project; git pull
+
+#Peddie Women in Tech
+cd; cd /var/www/CSProjects/clubs/women-in-tech; git pull
