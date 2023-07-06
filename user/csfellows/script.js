0c5defc7b8e23174a42fae3dd898e4e7c3c32fa6
Tomaz Chevres <tchevres-24@peddie.org>
2023-07-06 14:20:09 -0400

    Update client schedule on edit

--- user/csfellows/script.js
+++ user/csfellows/script.js
@@ -250,6 +250,8 @@ function signup(date,hour){
     },function (res) {
         console.log(res);
         if(res.message=='success'){
+            console.log(loadedMonths.get([date.getFullYear(), date.getMonth()].toString()));
+
             document.getElementById('day-'+date.getDate()).innerHTML += `<div class="event" id="event-${'event-'+res.id}" style="background-color:${stringToColor(userData.email)}; border-color:#00000000" >${userData.first_name+' '+userData.last_name}</div>`;
         }
     });
