96307288ec3b3bae425c2ac1af698ce98145a967
philiphan0109 <51898400+philiphan0109@users.noreply.github.com>
2023-09-18 09:25:09 -0400

    C:/Program Files/Git/admin/permissions/remove

--- nodejs/app.js
+++ nodejs/app.js
@@ -583,7 +583,7 @@ app.post('/admin/permissions/remove', (req, res) => {
                 con.query(`SELECT permissions FROM members WHERE email="${userEmail}"`, function (err, result, fields) {
                     //get current permissions
                     if (err) throw err;
-                    console.log(result[0]);
+                    console.log(result[0].permissions);
                     if (result[0].permissions.contains(perm)) {
                         let permArr = result[0].permissions.split(",");
                         for (let i = permArr.length - 1; i >= 0; i--) {
