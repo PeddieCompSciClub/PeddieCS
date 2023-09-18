1a1e3af26ed2831695afc3f3cc804012877f4e1f
philiphan0109 <51898400+philiphan0109@users.noreply.github.com>
2023-09-18 10:07:15 -0400

    C:/Program Files/Git/admin/permissions/remove

--- user/admin/admin.js
+++ user/admin/admin.js
@@ -61,7 +61,7 @@ function loadmembers() {
             //set listeners for permissions
             document.getElementById('permissionsearch-studentperms').addEventListener("keypress", function (event) {
                 if (event.key === "Enter") {
-                    console.log(loadedPermissionMember, event, this.value);
+                    addUserPermission(loadedPermissionMember, permission);
                 }
             });
 
