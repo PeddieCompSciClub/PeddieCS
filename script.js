9cbbdd0525e0ab2c7c180901c5230166fa360ac5
Tomaz Chevres <tchevres-24@peddie.org>
2023-10-09 00:10:13 -0400

    csfellows schedule

--- script.js
+++ script.js
@@ -170,10 +170,18 @@ function loadFellowsPreview(email,name,datetime,duration,location){
             </div>
         </div>`;
 }
+
+const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
 function loadFellowsSchedule(){
     loadFellowsScheduleJSON().then((response) => {
         let schedule = response.schedule;
-        console.log(schedule);
+        // console.log(schedule);
+        element = document.createElement('div');
+        for(let i=0; i<dayNames.length; i++){
+            if(i=0 && schedule["Sunday"].length==0) i++;
+            console.log(schedule[dayNames[i]]);
+        }
     });
 }
 
