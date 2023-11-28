d3bac39cc8f5cdc9e6da9ca334fe78773b823b91
PeddieCompSciClub <compsciclub@peddie.org>
2023-11-28 14:48:36 -0500

    email scheduler, get current datetime

--- nodejs/app.js
+++ nodejs/app.js
@@ -786,6 +786,7 @@ function scheduleFellowsReminder() {
 function emailFellowsReminder() {
     //set date as eastern time (complicated b/c of daylight savings)
     const date = new Date();
+    console.log(date.toLocaleTimeString('en-US', { timeZoneName: 'short', timeZone: 'America/New_York' }).split(' E')[0]);
     const date2 = new Date(date.toLocaleTimeString('en-US', { timeZoneName: 'short', timeZone: 'America/New_York' }).split(' E')[0]); //detect ex. 2:43:18 PM EST
     // const date2 = new Date(date.getTime() - etOffsetMinutes * 60000);
     console.log('Original Date (UTC):', date.toISOString());
